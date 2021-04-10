84e5075f05cc0c10ba95384bc633cf2c9c12e6fc
Sarah Duval <sarah.duval96@hotmail.fr>
2021-04-10 14:43:44 +0200

    Ajout commentaires formulaire.js

--- front-end/js/formulaire.js
+++ front-end/js/formulaire.js
@@ -1,8 +1,13 @@
+//Ecoute du btn "valider le formulaire"
 document.getElementById("form-submit").addEventListener("click", function() {
+    
+    //Récupération du formulaire
+    let form = document.getElementById("form-contact")
 
-let form = document.getElementById("form-contact")
-
+    //FormData représentant les champs du formualire et leur valeur
     let getForm = new FormData(form)
+
+    //Récupération de l'objet contact et du tableau des produits(id)
     var body = {
         contactObjec : {
         firstName : getForm.get("prenom"),
@@ -20,9 +25,7 @@ for (i = 0; i < basketArray.length; i++) {
     body.productsArray.push(tableauPaniers[i].id)
 }
 
-
-
-
+//Requête POST pour envoyer le formulaire et le panier au serveur
     fetch('http://localhost:3000/api/order', {
     method: 'post',
     headers: {
