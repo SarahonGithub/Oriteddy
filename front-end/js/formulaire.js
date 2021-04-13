9cf880e1cfd73f7067fa943b234f0a7ef595784d
Sarah Duval <sarah.duval96@hotmail.fr>
2021-04-13 16:10:43 +0200

    Modifs formulaire contact

--- front-end/js/formulaire.js
+++ front-end/js/formulaire.js
@@ -1,42 +1,38 @@
 //Ecoute du btn "valider le formulaire"
-document.getElementById("form-submit").addEventListener("click", function() {
-    
-    //Récupération du formulaire
-    let form = document.getElementById("form-contact")
+document.getElementById("form-submit").addEventListener("click", function(event) {
 
-    //FormData représentant les champs du formualire et leur valeur
-    let getForm = new FormData(form)
+    event.preventDefault();
 
     //Récupération de l'objet contact et du tableau des produits(id)
-    var body = {
-        contactObjec : {
-        firstName : getForm.get("prenom"),
-        lastName : getForm.get("nom"),
-        adress : getForm.get("adress"),
-        city : getForm.get("ville"),
-        email : getForm.geet("email")
+    let body = {
+        contact : {
+            firstName : document.getElementById("firstName").value,
+            lastName : document.getElementById("lastName").value,
+            adress : document.getElementById("adress").value,
+            city : document.getElementById("city").value,
+            email : document.getElementById("email").value
     },
-    productsArray : []
+    products : []
 }
 
 let basketArray = JSON.parse(localStorage.getItem('selectedTeddies'))
 //Parcourir le panier
 for (i = 0; i < basketArray.length; i++) {
-    body.productsArray.push(tableauPaniers[i].id)
+    body.products.push(tableauPaniers[i].id)
 }
 
 //Requête POST pour envoyer le formulaire et le panier au serveur
-    fetch('http://localhost:3000/api/order', {
+    fetch('http://localhost:3000/api/teddies/order', {
     method: 'post',
     headers: {
         'Content-type': 'application/json; charset=UTF-8'
       },
     body: body
-})
-  .then(json)
-  .then(function (data) {
-    console.log('Request succeeded with JSON response', data);
-  })
+}).then(function (response) {
+    return response.json();
+}).then function(json)
+console.log(json)
+
   .catch(function (error) {
     console.log('Request failed', error);
   })
